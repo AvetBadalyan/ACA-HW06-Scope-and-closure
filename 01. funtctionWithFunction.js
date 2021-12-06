25e54c3a8148568a2327d9df94a8af346945a4b5
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-06 23:02:15 +0400

    feat: calculate sum of numbers with F in F

--- 01. funtctionWithFunction.js
+++ 01. funtctionWithFunction.js
@@ -6,3 +6,10 @@ function sum(a) {
 }
 
 console.log(sum(1)(2));
+
+const addOne = sum(1);
+console.log(addOne(2));
+const addTen = sum(10);
+console.log(addTen(2)); // 12
+console.log(addOne(4)); // 5
+console.log(addTen(10)); // 20)
