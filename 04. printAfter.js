1ad4bccf26e7a8fd29a5e71fea3e07f605a115a7
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-06 23:44:02 +0400

    feat" Print argument after printing Hello World

---
+++ 04. printAfter.js
@@ -0,0 +1,7 @@
+//Create a function printAfter that calls its argument after printing hello world
+function printAfter(argument) {
+  console.log("Hello World");
+  console.log(argument);
+}
+
+printAfter("Elon Musk");
