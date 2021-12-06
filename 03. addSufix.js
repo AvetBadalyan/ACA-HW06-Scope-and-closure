9c795380fdd5d63867c0d8ee1f91eb3403add219
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-06 23:35:34 +0400

    feat: add suffix in the end of the string

---
+++ 03. addSufix.js
@@ -0,0 +1,11 @@
+//Write a function that returns an anonymous function, which transforms its input by adding a
+//particular suffix at the end.
+function add_suffix(stringArr, suffix) {
+  let newString = stringArr.map(function (element) {
+    element.concat(suffix);
+  });
+  return newString;
+}
+
+console.log(add_suffix(["hopeless", "total"], "ly"));
+//need to fix bugs, returns undefined
