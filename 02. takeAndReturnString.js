457eb11e36460ac8424003a82ee10edf79061874
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-09 21:40:34 +0400

    feat: F that returns a F with the given string

--- 02. takeAndReturnString.js
+++ 02. takeAndReturnString.js
@@ -1,8 +1,8 @@
 // Write a function redundant that takes in a string str and returns a function that returns str.
 
-function returnRedundant(string) {
+function returnRedundant(str) {
   return function internFunction() {
-    return string;
+    return str;
   };
 }
 console.log(returnRedundant("My name is Avet")());
