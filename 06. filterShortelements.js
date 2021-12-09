7609bef7f55f5d2c6a39023ab08acdfc5c0e72b2
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-09 22:10:17 +0400

    feat: filter el-s shorter than 3 letters in name

--- 06. filterShortelements.js
+++ 06. filterShortelements.js
@@ -1,8 +1,8 @@
 // Write a function which remove elements with length <= 3
 function filterByLength(arr) {
   let newArr = arr.filter(function (element) {
-      return element.length > 3;
+    return element.length > 3;
   });
+  return newArr;
 }
 console.log(filterByLength(["kia", "tesla", "bmw", "mercedes"]));
-//NEED TO FIX BUGS OF UNDEFINED
